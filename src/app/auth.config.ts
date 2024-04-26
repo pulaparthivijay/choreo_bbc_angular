2454ddeb923bd1d8d731c33d8099b181e9e011fb
vijay <vijay.p@massiltech.com>
2024-04-26 15:57:53 +0530

    Update auth.config.ts

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -1,15 +1,15 @@
 import { AuthConfig } from 'angular-oauth2-oidc';
 
 export const authCodeFlowConfig: AuthConfig = {
-    issuer: 'https://api.asgardeo.io/t/massil2016/oauth2/token',
+    issuer: 'https://api.asgardeo.io/t/yudhistertech/oauth2/token',
     // clientId: 'scUl4btnf6Wujm3pdiJgGWwV8IEa',
-    clientId: 'Efa4ijXGqcD9GbuA4Ol8AuDWpY4a',
+    clientId: 'oS9fHchu913fj3beqY4k0cafAC4a',
     responseType: 'code',
     redirectUri: 'https://b9eb8320-92a5-4ca3-aab7-1e86706546a4.e1-us-east-azure.choreoapps.dev/dashboard',
 //    redirectUri:'http://localhost:4200/dashboard',
-    tokenEndpoint: 'https://api.asgardeo.io/t/massil2016/oauth2/token',
+    tokenEndpoint: 'https://api.asgardeo.io/t/yudhistertech/oauth2/token',
     skipIssuerCheck: true,
-    logoutUrl: 'https://api.asgardeo.io/t/massil2016/oidc/logout',
+    logoutUrl: 'https://api.asgardeo.io/t/yudhistertech/oidc/logout',
     postLogoutRedirectUri: 'https://b9eb8320-92a5-4ca3-aab7-1e86706546a4.e1-us-east-azure.choreoapps.dev/dashboard',
     // postLogoutRedirectUri: 'http://localhost:4200/dashboard',
     scope: 'openid profile internal_user_mgt_view', 
